d8e5a677ba3c95139bd0428779247f537a5cee27
adrianscarlett <adrian.scarlett@codethink.co.uk>
2025-06-02 17:35:32 +0100

    fixes

--- lib/import-all.nix
+++ lib/import-all.nix
@@ -1,18 +1,11 @@
-# Import all .nix files from a directory
+# Import all .nix files from a directory as a set
 dir: let
-  # Get all .nix files
-  files = if builtins.pathExists dir
-    then builtins.attrNames (builtins.readDir dir)
-    else [];
-
-  # Filter for .nix files
+  inherit (builtins) readDir pathExists attrNames;
+  files = if pathExists dir then attrNames (readDir dir) else [];
   nixFiles = builtins.filter (f: builtins.match ".*\.nix" f != null) files;
-
-  # Import each file
-  imported = map (f: import (dir + "/${f}")) nixFiles;
-
-  # Merge all imported files
-  mergeInputs = builtins.foldl' (acc: input: acc // input) {};
-
+  importFile = f: 
+    let val = import (dir + "/${f}");
+    in if builtins.isFunction val then val {} else val;
+  merged = builtins.foldl' (a: b: a // b) {} (map importFile nixFiles);
 in
-  mergeInputs imported
+  merged
