3d95870ca8d039c5304f66f34c2a738d8eb4e7d2
adrianscarlett <adrian.scarlett@codethink.co.uk>
2025-06-02 17:33:17 +0100

    fixes

--- lib/import-all.nix
+++ lib/import-all.nix
@@ -1,16 +1,5 @@
 # Import all .nix files from a directory
 dir: let
-  # Helper function to merge two attribute sets
-  recursiveMerge = a: b: 
-    let
-      f = name:
-        if builtins.hasAttr name a && builtins.hasAttr name b &&
-           builtins.isAttrs a.${name} && builtins.isAttrs b.${name}
-        then recursiveMerge a.${name} b.${name}
-        else b.${name};
-    in
-    a // (builtins.mapAttrs f b);
-
   # Get all .nix files
   files = if builtins.pathExists dir
     then builtins.attrNames (builtins.readDir dir)
@@ -19,15 +8,11 @@ dir: let
   # Filter for .nix files
   nixFiles = builtins.filter (f: builtins.match ".*\.nix" f != null) files;
 
-  # Import each file and handle functions
-  imported = map (f: 
-    let 
-      raw = import (dir + "/${f}");
-      # If it's a function, call it with an empty set
-      value = if builtins.isFunction raw then raw {} else raw;
-    in value
-  ) nixFiles;
+  # Import each file
+  imported = map (f: import (dir + "/${f}")) nixFiles;
+
+  # Merge all imported files
+  mergeInputs = builtins.foldl' (acc: input: acc // input) {};
 
 in
-  # Merge all imported files
-  builtins.foldl' recursiveMerge {} imported
+  mergeInputs imported
