d6f64746e02b7153c9c7180b67c1093ff5ab3d9d
adrianscarlett <adrian.scarlett@codethink.co.uk>
2025-07-17 12:02:37 +0100

    fixups

--- outputs/home-configurations.nix
+++ outputs/home-configurations.nix
@@ -1,7 +1,8 @@
 inputs:
 {
   homeConfigurations = let
-    homes = (import ../lib/mk-homes.nix { lib = inputs.nixpkgs.lib; }) ../homes;
+    mkConfigs = import ../lib/mk-configs.nix { lib = inputs.nixpkgs.lib; };
+    homes = mkConfigs.mkHomes ../homes;
     mkHome = name: homeConfig: inputs.home-manager.lib.homeManagerConfiguration {
       pkgs = inputs.nixpkgs.legacyPackages.x86_64-linux;
       modules = [
