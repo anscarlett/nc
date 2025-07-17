34be51b8a27480000396eb3736a6ad8d7b2240f6
adrianscarlett <adrian.scarlett@codethink.co.uk>
2025-07-17 11:17:39 +0100

    fixups

--- outputs/nixos-configurations.nix
+++ outputs/nixos-configurations.nix
@@ -1,15 +1,20 @@
 inputs: {
   nixosConfigurations = let
-    # Get all hosts from the directory structure
     hosts = (import ../lib/mk-hosts.nix { lib = inputs.nixpkgs.lib; }) ../hosts;
-    
-    # Create a NixOS system for each host
     mkHost = name: hostConfig: inputs.nixpkgs.lib.nixosSystem {
       inherit (hostConfig) system;
       modules = hostConfig.modules ++ [
         inputs.agenix.nixosModules.default
       ];
     };
+    allHosts = hosts // {
+      installer = {
+        system = "x86_64-linux";
+        modules = [
+          ../hosts/installer/default.nix
+        ];
+      };
+    };
   in
-    builtins.mapAttrs mkHost hosts;
+    builtins.mapAttrs mkHost allHosts;
 }
