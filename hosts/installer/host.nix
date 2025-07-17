485d1f7938d8736b3d688db09ea6c50c3c8519c7
adrianscarlett <adrian.scarlett@codethink.co.uk>
2025-07-17 11:23:31 +0100

    fixups

--- hosts/installer/host.nix
+++ hosts/installer/host.nix
@@ -1,3 +1,8 @@
+{ config, pkgs, ... }:
 {
-  
+  # Minimal NixOS installer configuration
+  boot.loader.grub.enable = false;
+  services.sshd.enable = true;
+  users.users.root.password = "nixos";
+  # Add any other installer-specific options you want
 }
