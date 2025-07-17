c5dd28abdebf2a03a0c5268f3db33ec390fa303a
adrianscarlett <adrian.scarlett@codethink.co.uk>
2025-07-17 12:13:18 +0100

    fixups

--- outputs/home-configurations.nix
+++ outputs/home-configurations.nix
@@ -1,13 +1,14 @@
-inputs: {
-  let
-    mkConfigs = import ../lib/mk-configs.nix { lib = inputs.nixpkgs.lib; };
-    homes = mkConfigs.mkHomes ../homes;
-    mkHome = name: homeConfig: inputs.home-manager.lib.homeManagerConfiguration {
-      pkgs = inputs.nixpkgs.legacyPackages.x86_64-linux;
-      modules = [
-        (homeConfig inputs)
-      ];
-    };
-  in
-    builtins.mapAttrs mkHome homes;
+inputs:
+let
+  mkConfigs = import ../lib/mk-configs.nix { lib = inputs.nixpkgs.lib; };
+  homes = mkConfigs.mkHomes ../homes;
+  mkHome = name: homeConfig: inputs.home-manager.lib.homeManagerConfiguration {
+    pkgs = inputs.nixpkgs.legacyPackages.x86_64-linux;
+    modules = [
+      (homeConfig inputs)
+    ];
+  };
+in
+{
+  builtins.mapAttrs mkHome homes;
 }
--- outputs/nixos-configurations.nix
+++ outputs/nixos-configurations.nix
@@ -1,21 +1,22 @@
-inputs: {
-  let
-    mkConfigs = import ../lib/mk-configs.nix { lib = inputs.nixpkgs.lib; };
-    hosts = mkConfigs.mkHosts ../hosts;
-    mkHost = name: hostConfig: inputs.nixpkgs.lib.nixosSystem {
-      inherit (hostConfig) system;
-      modules = hostConfig.modules ++ [
-        inputs.agenix.nixosModules.default
+inputs:
+let
+  mkConfigs = import ../lib/mk-configs.nix { lib = inputs.nixpkgs.lib; };
+  hosts = mkConfigs.mkHosts ../hosts;
+  mkHost = name: hostConfig: inputs.nixpkgs.lib.nixosSystem {
+    inherit (hostConfig) system;
+    modules = hostConfig.modules ++ [
+      inputs.agenix.nixosModules.default
+    ];
+  };
+  allHosts = hosts // {
+    installer = {
+      system = "x86_64-linux";
+      modules = [
+        ../hosts/installer/host.nix
       ];
     };
-    allHosts = hosts // {
-      installer = {
-        system = "x86_64-linux";
-        modules = [
-          ../hosts/installer/host.nix
-        ];
-      };
-    };
-  in
-    builtins.mapAttrs mkHost allHosts;
+  };
+in
+{
+  builtins.mapAttrs mkHost allHosts;
 }
