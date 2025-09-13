65e2ea7de121dab547d5ecfd358f64faef341c8e
Skalamanga <anscarlett@gmail.com>
2025-09-13 13:54:33 +0100

    Make bare branch

--- old/lib/default.nix
+++
@@ -1,7 +0,0 @@
-# Library functions for the NixOS configuration
-{
-  auto-users = import ./auto-users.nix;
-  get-name-from-path = import ./get-name-from-path.nix;
-  mk-configs = import ./mk-configs.nix;
-  validate-device = import ./validate-device.nix;
-}
--- old/lib/validate-device.nix
+++
@@ -1,4 +0,0 @@
-device:
-  if device == null || device == "" || !builtins.match "^/dev/disk/by-id/.+" device != null
-    then abort "Error: 'device' must be set to a valid /dev/disk/by-id/... path"
-    else device
